5d239eb3cf1f8be0e83c8d7d03168d87e71b069c
Doria2002 <164808610+Doria2002@users.noreply.github.com>
2024-05-17 01:46:24 +0200

    Update PageVisiteur.php

--- PageVisiteur.php
+++ PageVisiteur.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Page D'accueil visiteur Size Group</title>
-    <link rel="stylesheet" href="style5.css">
+    <link rel="stylesheet" href="PageVisiteur.css">
 </head>
 <body>
     <header>
